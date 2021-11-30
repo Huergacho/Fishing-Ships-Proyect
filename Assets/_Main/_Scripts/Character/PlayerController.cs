cecf36cc1fc2afe7ca5c55f7d40de544c0be2e7b
Huergacho <75048402+Huergacho@users.noreply.github.com>
2021-11-30 15:21:22 -0300

    Main Movement

---
+++ Assets/_Main/_Scripts/Character/PlayerController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Vector3 direction;
+    [SerializeField] private float rotSpeed;
+    [SerializeField] private Transform mouseIndicator;
+    private Vector3 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveWithMouseInput();
+        MoveMouseIndicator();
+    }
+    Ray CalculateMousePos()
+    {
+        return Camera.main.ScreenPointToRay(Input.mousePosition);
+    }
+    void LookAtMousePosition()
+    {
+        
+        if(Physics.Raycast(CalculateMousePos(), out RaycastHit hitInfo))
+        {
+            target = hitInfo.point;
+            target.y = 0;
+            var distance = target - transform.position;
+            if(distance.magnitude >= 0.05)
+            {
+                transform.LookAt(target);
+            }
+        }
+    }
+    void MoveWithMouseInput()
+    {
+        LookAtMousePosition();
+        if (Input.GetMouseButton(1))
+        {
+            var distance = target - transform.position;
+            if (distance.magnitude >= 0.05)
+                transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        }
+    }
+    void MoveWithKeys()
+    {
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        transform.position += direction * speed * Time.deltaTime;
+    }
+    void MoveMouseIndicator()
+    {
+        mouseIndicator.position = target;
+    }
+}
---
+++ Assets/_Main/_Scripts/FMS/State.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State<T> : IState<T>
+{
+    Dictionary<T, IState<T>> _stateTransitions = new Dictionary<T, IState<T>>();
+    FSM<T> _parentFSM;
+    public FSM<T> parentFSM { get => _parentFSM; set => _parentFSM = value; }
+    public void Awake()
+    {
+ 
+    }
+
+    public void Execute()
+    {
+        
+    }
+    public void Sleep()
+    {
+        
+    }
+    public void AddTransition(T input, IState<T> transitionState)
+    {
+        if (!_stateTransitions.ContainsKey(input))
+        {
+            _stateTransitions[input] = transitionState;
+        }
+    }
+
+    public IState<T> GetTransition(T input)
+    {
+        if (_stateTransitions.ContainsKey(input))
+        {
+            return _stateTransitions[input];
+        }
+        return null;
+    }
+
+    public void RemoveTransition(T input)
+    {
+        if (_stateTransitions.ContainsKey(input))
+        {
+        _stateTransitions.Remove(input);
+        }
+    }
+
+    public void RemoveTransition(IState<T> state)
+    {
+        if (_stateTransitions.ContainsValue(state))
+        {
+            foreach (var item in _stateTransitions)
+            {
+                if (item.Value == state)
+                {
+                    _stateTransitions.Remove(item.Key);
+                }
+            }
+        }
+    }
+
+}
